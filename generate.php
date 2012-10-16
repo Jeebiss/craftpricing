ec96e5c29cfcf5c6c3caaaa0a0d473ab76e80e60
Mike Botsko <botsko@gmail.com>
2012-10-16 10:56:08 -0700

    adding bool for base items, more price changes

--- generate.php
+++ generate.php
@@ -14,7 +14,7 @@
 		
 		TRUNCATE TABLE `craftys_products`;<br>
 		<?php if($items): foreach($items as $item): ?>
-		INSERT INTO `craftys_products` (`item_id`, `sub_id`, `alias`, `item`, `buy_price`, `sell_price`, `allows_user_sale`) VALUES ("<?= $item['item_id'] ?>","<?= $item['sub_id'] ?>","<?= $item['alias'] ?>","<?= $item['name'] ?>",<?= $item['buy'] ?>,<?= $item['sell'] ?>,1);<br>
+		INSERT INTO `craftys_products` (`item_id`, `sub_id`, `alias`, `item`, `buy_price`, `sell_price`, `allows_user_sale`,`is_base_item`) VALUES ("<?= $item['item_id'] ?>","<?= $item['sub_id'] ?>","<?= $item['alias'] ?>","<?= $item['name'] ?>",<?= $item['buy'] ?>,<?= $item['sell'] ?>,1,<?= empty($item['recipe']) ? 1 : 0 ?>);<br>
 		<?php endforeach; endif; ?>
 
 	</body>
